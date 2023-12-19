4ec354c36a6e87b09addf1221bd5a898284c6d66
hdoid <ddharmaesa@gmail.com>
2023-12-19 22:05:30 +0800

    dikir

---
+++ dikir/database/seeders/CourierSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Courier;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() : void
+    {
+        Courier::insert([
+            ['code' => 'jne', 'title' => 'Jalur Nugraha Ekakurir (JNE)'],
+            ['code' => 'pos', 'title' => 'POS Indonesia'],
+            ['code' => 'tiki', 'title' => 'Citra Van Titipan Kilat (TiKi)'],
+            ['code' => 'j&t', 'title' => 'Jet & Tony (J&T)'],
+            ['code' => 'sicepat', 'title' => 'SiCepat Express']
+        ]);
+    }
+}
